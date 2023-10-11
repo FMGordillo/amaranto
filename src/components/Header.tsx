8ced636c0bccef75ef93e5e4da8c0d93be7c0716
Facundo Martin Gordillo <me@facundogordillo.com>
2023-10-11 23:24:03 +0200

    fix: forgot to add login

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { useSession } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import Link from "next/link";
 
 export default function Header() {
@@ -8,7 +8,7 @@ export default function Header() {
     <nav className="bg-fuchsia-700 p-4 text-white">
       <div className="container mx-auto flex items-center justify-between">
         <Link href="/"><h1 className="text-2xl font-bold">Amaranto</h1></Link>
-        <span>{session ? "Hi!" : <button>Login</button>}</span>
+        <span>{session ? "Hi!" : <button onClick={() => void signIn()}>Login</button>}</span>
       </div>
     </nav>
   );
